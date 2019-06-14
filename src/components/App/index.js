a9a6b115b3784593b9339dece830c27eff1aef8b
AiaRup <aiale88@gmail.com>
2019-06-14 12:20:25 +0200

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class App extends Component {
+  render() {
+    return <div className="App">app</div>;
+  }
+}
+
+export default App;
