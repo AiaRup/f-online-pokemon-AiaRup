66302add119bc90d30d4885bc57bf22b49d5a488
AiaRup <aiale88@gmail.com>
2019-06-16 12:44:59 +0200

    add new docs folder

---
+++ docs/precache-manifest.de415b275517b6b196f8e0272766b9de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce9c09033ff78bbec25240963748ece",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f636c89bb9af9eb16961",
+    "url": "./static/css/main.3528e4c8.chunk.css"
+  },
+  {
+    "revision": "00063f64c68494124901",
+    "url": "./static/js/2.49afd6f5.chunk.js"
+  },
+  {
+    "revision": "f636c89bb9af9eb16961",
+    "url": "./static/js/main.a71325f7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6e5393a64ca23c455e8568d409e82da6",
+    "url": "./static/media/Pokemon Hollow.6e5393a6.ttf"
+  },
+  {
+    "revision": "15df305c09180456b09901932d08e8e8",
+    "url": "./static/media/pokemon-image.15df305c.png"
+  }
+]);
